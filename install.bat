ba50f64e480721d0520a93264e76ff5393e7209b
Matthew McGowan <mat.mcgowan+github@gmail.com>
2016-04-19 01:21:18 +0200

    splits dependencies into platform neutral and platform specific.  removes "master" only build since tagged builds has the branch name the same as the tag.

--- install.bat
+++ install.bat
@@ -6,6 +6,7 @@ call venv/Scripts/activate.bat
 
 REM pip install -I Cython==0.23
 pip install -r requirements.txt
+pip install -r requirements-win.txt
 
 cd installer
 python -m PyInstaller -y --onefile win_onefile.spec
