ca262e83294b5bb837e38037b16f4f68882d709f
Matthew McGowan <mat.mcgowan+github@gmail.com>
2016-05-10 20:46:47 +0200

    rename to particle_updater on OS X

--- installer/package.sh
+++ installer/package.sh
@@ -1,11 +1,11 @@
 python -m PyInstaller  -y --windowed --icon=resources/particle.icns osx_onefile.spec
-rm -rf particle_system_firmware.app
+rm -rf particle_updater.app
 pushd dist
 signid="TNJ67X9MQD"
-codesign -s $signid particle_system_firmware.app/Contents/MacOS/particle_system_firmware
-codesign -s $signid --force --verify --verbose particle_system_firmware.app
-rm particle_system_firmware.zip
-zip -r particle_system_firmware.zip particle_system_firmware.app
+codesign -s $signid particle_updater.app/Contents/MacOS/particle_updater
+codesign -s $signid --force --verify --verbose particle_updater.app
+rm particle_updater.zip
+zip -r particle_updater.zip particle_updater.app
 
-sudo spctl -a -v particle_system_firmware.app
+sudo spctl -a -v particle_updater.app
 popd 
