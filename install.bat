ff7df14ae205b45115f6b02fea0e9891f55453a5
Matthew McGowan <mat.mcgowan+github@gmail.com>
2016-06-09 02:15:07 +0200

    separate setup from main build

--- install.bat
+++ install.bat
@@ -1,7 +1,4 @@
-set PATH=%PYTHON%;%PYTHON%/scripts;%PATH%
-pip install --disable-pip-version-check --user --upgrade pip
-pip install virtualenv
-virtualenv venv
+
 call venv/Scripts/activate.bat
 
 REM pip install -I Cython==0.23
@@ -9,7 +6,6 @@ pip install -r requirements.txt
 pip install -r requirements-win.txt
 
 cd installer
-python -m PyInstaller -y --onefile win_onefile.spec --clean --manifest manifest.xml
-"%SIGNTOOL_PATH%\signtool.exe" sign /v /f ../cert.p12 /p %cer_secret% /tr http://tsa.starfieldtech.com dist\particle_system_firmware.exe
+package.bat
 cd ..
  
--- installer/package.bat
+++ installer/package.bat
@@ -1,13 +1,14 @@
 if "%1"=="" goto noversion
 
 set VERSION=%1
-set APPNAME=particle_device_upgrader
+set APPNAME=particle_firmware_manager
 set FQNAME=%APPNAME%-%VERSION%-windows
 
 rd /s /y dist
 
 python -m PyInstaller -y --onefile win_onefile.spec --clean
-call sign.bat
+
+"%SIGNTOOL_PATH%signtool.exe" sign /v /f windows_key.p12 /p %key_secret% /tr http://tsa.starfieldtech.com dist\%FQNAME%.exe
 goto done
 
 :noversion
