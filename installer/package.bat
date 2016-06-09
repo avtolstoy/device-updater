ca62872d8096ec478962ab1644cb4d40afee3e10
Matthew McGowan <mat.mcgowan+github@gmail.com>
2016-06-09 18:01:25 +0200

    re-introduce /y - even though windows complains it's an unrecognized parameter, it hangs the build without it, prompting "are you sure? Y/N"

--- installer/package.bat
+++ installer/package.bat
@@ -4,7 +4,7 @@ set VERSION=%1
 set APPNAME=particle_firmware_manager
 set FQNAME=%APPNAME%-%VERSION%-windows
 
-rd /s dist
+rd /y /s dist
 
 python -m PyInstaller -y --onefile win_onefile.spec --clean
 
