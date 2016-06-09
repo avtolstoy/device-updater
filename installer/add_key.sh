6c32d1ffb23a63582d9fae79ace9737167815a07
Matthew McGowan <mat.mcgowan+github@gmail.com>
2016-06-10 00:54:18 +0200

    add keychain as default

--- installer/add_key.sh
+++ installer/add_key.sh
@@ -3,4 +3,5 @@ security create-keychain -p $XCODE_KEYCHAIN_PASSWORD $XCODE_KEYCHAIN
 security import cert.cer -k ~/Library/Keychains/$XCODE_KEYCHAIN -T /usr/bin/codesign
 security import key.p12  -x -k ~/Library/Keychains/$XCODE_KEYCHAIN -P $KEY_PASSWORD -T /usr/bin/codesign
 security list-keychains -d user -s login.keychain $XCODE_KEYCHAIN
+security default-keychain -s $XCODE_KEYCHAIN
 
