bbbcf28ed06ea12b3dabbcdb211f19f69f39462d
Matthew McGowan <mat.mcgowan+github@gmail.com>
2016-06-09 22:40:51 +0200

    create keychain

--- installer/add_key.sh
+++ installer/add_key.sh
@@ -1,5 +1,6 @@
 XCODE_KEYCHAIN="${XCODE_KEYCHAIN:-build}"
+security create-keychain -p $XCODE_KEYCHAIN_PASSWORD $XCODE_KEYCHAIN
 security import cert.cer -k ~/Library/Keychains/$XCODE_KEYCHAIN -T /usr/bin/codesign
-security import key.p12 -k ~/Library/Keychains/$XCODE_KEYCHAIN -P $KEY_PASSWORD -T /usr/bin/codesign
+security import key.p12  -x -k ~/Library/Keychains/$XCODE_KEYCHAIN -P $KEY_PASSWORD -T /usr/bin/codesign
 security list-keychains -d user -s login.keychain $XCODE_KEYCHAIN
 
