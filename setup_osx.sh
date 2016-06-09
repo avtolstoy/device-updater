d7e7041edf9adb728932dfe77986f74c924878af
Matthew McGowan <mat.mcgowan+github@gmail.com>
2016-06-09 04:30:44 +0200

    configures pyenv

--- setup_osx.sh
+++ setup_osx.sh
@@ -3,5 +3,10 @@ pyver=3.5.1
 brew update
 brew outdated pyenv || brew upgrade pyenv
 PYTHON_CONFIGURE_OPTS="--enable-framework" pyenv install $pyver -s
+
+export PYENV_ROOT=/usr/local/opt/pyenv
+export PATH=$PYENV_ROOT:$PATH
+eval "$(pyenv init -)"
+
 pyenv rehash
 pyenv local $pyver
