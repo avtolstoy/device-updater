25bc2c8a58980366de8a88833ee26d90beef1bd6
Matthew McGowan <mat.mcgowan+github@gmail.com>
2016-06-09 03:29:37 +0200

    pass version to packager

--- install.bat
+++ install.bat
@@ -5,7 +5,10 @@ REM pip install -I Cython==0.23
 pip install -r requirements.txt
 pip install -r requirements-win.txt
 
+SET VERSION=%APPVEYOR_REPO_TAG_NAME%
+IF "%VERSION%"=="" SET VERSION=%APPVEYOR_REPO_COMMIT%
+
 cd installer
-package.bat
+package.bat %VERSION%
 cd ..
  
