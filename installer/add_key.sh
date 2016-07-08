6fa97012ef9c64f8751600a0543ed2b82c5ed7e1
Matthew McGowan <mat.mcgowan+github@gmail.com>
2016-07-08 02:08:43 +0200

    attempt to unblock travis by running as sudo

--- installer/add_key.sh
+++ installer/add_key.sh
@@ -1,4 +1,4 @@
-security create-keychain -p $XCODE_KEYCHAIN_PASSWORD $XCODE_KEYCHAIN
-security import cert.cer -k ~/Library/Keychains/$XCODE_KEYCHAIN -T /usr/bin/codesign
-security import key.p12  -x -k ~/Library/Keychains/$XCODE_KEYCHAIN -P $KEY_PASSWORD -T /usr/bin/codesign
-security list-keychains -d user -s login.keychain $XCODE_KEYCHAIN
+sudo security create-keychain -p $XCODE_KEYCHAIN_PASSWORD $XCODE_KEYCHAIN
+sudo security import cert.cer -k ~/Library/Keychains/$XCODE_KEYCHAIN -T /usr/bin/codesign
+sudo security import key.p12  -x -k ~/Library/Keychains/$XCODE_KEYCHAIN -P $KEY_PASSWORD -T /usr/bin/codesign
+sudo security list-keychains -d user -s login.keychain $XCODE_KEYCHAIN
