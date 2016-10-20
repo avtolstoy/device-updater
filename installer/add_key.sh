8c60f7c818b0f3acf3c3149d2aa5850509098d57
Matthew McGowan <mat.mcgowan+github@gmail.com>
2016-10-20 05:44:31 +0200

    ensure the keychain is unlocked so it can be used freely

--- installer/add_key.sh
+++ installer/add_key.sh
@@ -6,3 +6,5 @@ echo "Importing key"
 sudo security import key.p12  -x -k ~/Library/Keychains/$XCODE_KEYCHAIN -P $KEY_PASSWORD  -T /usr/bin/codesign
 echo "List keychains"
 sudo security list-keychains -d user -s login.keychain $XCODE_KEYCHAIN
+echo "Unlocking keychain"
+sudo security unlock-keychain -p $XCODE_KEYCHAIN_PASSWORD $XCODE_KEYCHAIN
